ce297259e8787f2dd1583dd6870e879c61c7d74b
Fayaz Mohammed <fayaz0211@gmail.com>
2020-04-27 12:55:52 +0100

    added the functions in search by page to check if the search options been selected before searching

--- tests/acceptance-tests/pages/search_by_page.py
+++ tests/acceptance-tests/pages/search_by_page.py
@@ -4,13 +4,26 @@ from pages.base_page import BasePage
 
 
 class SearchByPage(BasePage):
-    REFERENCE = By.ID, 'reference'
+    REFERENCE = By.NAME, 'reference'
     PERIOD = By.NAME, 'period'
     SURVEY = By.NAME, 'survey'
     SEARCH_BUTTON = By.XPATH, "//button[@class='btn btn--small']"
 
     def select_search_by(self, reference_number):
-        ele = self.driver.find_element(*SearchByPage.REFERENCE)
-        ele.send_keys(reference_number)
+        self.select_search_by_options()
+        # ele = self.driver.find_element(*SearchByPage.REFERENCE)
+        # ele.send_keys(reference_number)
+        # elem = self.driver.find_element(*SearchByPage.SEARCH_BUTTON)
+        # elem.click()
+
+    def select_search_by_options(self):
+        self.is_checked(*SearchByPage.REFERENCE)
+        self.is_checked(*SearchByPage.PERIOD)
+        self.is_checked(*SearchByPage.SURVEY)
         elem = self.driver.find_element(*SearchByPage.SEARCH_BUTTON)
         elem.click()
+
+    def is_checked(self, *element):
+        element_to_be_checked = self.driver.find_element(*element)
+        if element_to_be_checked.get_attribute("checked") == "false":
+            self.driver.find_element(element_to_be_checked).click()
