4a83134fa324dd106be12a94b60ac5d6e9d583fc
Fayaz Mohammed <fayaz0211@gmail.com>
2020-05-06 08:23:12 +0100

    moved common function switch to alert  to selenium core

--- tests/acceptance-tests/base/selenium_core.py
+++ tests/acceptance-tests/base/selenium_core.py
@@ -1,3 +1,8 @@
+import time
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from base.driver_context import DriverContext
 
 
@@ -7,6 +12,29 @@ class SeleniumCore:
         ele = DriverContext.driver.find_element(*element)
         ele.clear()
         return ele
+
+    @staticmethod
+    def switch_to_alert_box():
+        # Click on the "Refresh" button to generate the Confirmation Alert
+        driver = DriverContext.driver
+        driver.refresh()
+        try:
+            # Switch the control to the Alert window
+            WebDriverWait(driver, 5).until(EC.alert_is_present(), 'Timed out waiting for alert')
+            alert = driver.switch_to.alert
+            # Retrieve the message on the Alert window
+            message = alert.text
+            print("Alert shows following message: " + message)
+            time.sleep(2)
+            # use the accept() method to accept the alert
+            alert.accept()
+            print("Alert accepted")
+            # get the text returned when OK Button is clicked.
+            txt = driver.find_element_by_id('msg')
+            print(txt.text)
+            time.sleep(2)
+        except TimeoutException:
+            print("No Alert")
 
     @staticmethod
     def switch_window():
