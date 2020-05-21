6a78ba677f60e3fc8381512aa09ebb2bcbdbeb2b
Fayaz Mohammed <fayaz0211@gmail.com>
2020-05-21 12:35:35 +0100

    added the condition to set the page load timeout till 10 seconds and fail tests afterwards

--- tests/acceptance-tests/base/browser.py
+++ tests/acceptance-tests/base/browser.py
@@ -2,7 +2,10 @@
 Class containing common functions used in several tests.
 Functions that are not test or feature specific.
 """
+import time
+
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 
 from base.driver_context import DriverContext
 from config_files.config_test import ConfigTest
@@ -21,17 +24,18 @@ class Browser:
         if not browser_type:
             # create instance of the Chrome driver
             DriverContext.driver = webdriver.Chrome(ConfigTest.CHROME_DRIVER_LOCATION)
+            DriverContext.driver.maximize_window()
+
         elif browser_type.lower() == 'firefox':
             # create instance of Firefox driver the browser type is not specified
             DriverContext.driver = webdriver.Firefox()
         else:
             raise Exception("The browser type '{}' is not supported".format(browser_type))
 
-        Browser.navigate_to_the_url()
+        # Browser.navigate_to_the_url()
 
     @staticmethod
     def navigate_to_the_url():
-
         # clean the url and go to the url
         # Create a class called url_configs in config_files folder
         # and create a dictionary list URL_CONFIG add a env_url as key and url as value
@@ -43,14 +47,21 @@ class Browser:
         # url = ConfigTest.UI_URL
         try:
             if url:
-                DriverContext.driver.get(url)
-                DriverContext.driver.maximize_window()
-                DriverContext.driver.implicitly_wait(2)
-                return DriverContext.driver
+                t = time.time()
+                DriverContext.driver.set_page_load_timeout(10)
+                try:
+                    current_url = DriverContext.driver.current_url
+                    if url != current_url:
+                        DriverContext.driver.get(url)
+                        return DriverContext.driver
+                except TimeoutException:
+                    DriverContext.driver.execute_script("window.stop();")
+                    assert False, 'Time taken to load the url: ' + str(time.time() - t)
             else:
                 raise ValueError(
                     'empty url.'
                     'Please set the url in url_configs file.'
                     ' Follow the steps mentioned in the comments')
+
         except ValueError as e:
             print(e)
