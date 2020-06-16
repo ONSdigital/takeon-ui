3e9c8eaa2b91a6c992b4c184eedb0fb2939f841f
Fayaz Mohammed <fayaz0211@gmail.com>
2020-06-16 10:07:30 +0100

    added missing parameter

--- tests/acceptance-tests/base/reporting_helper.py
+++ tests/acceptance-tests/base/reporting_helper.py
@@ -1,15 +1,15 @@
 class ReportingHelper:
 
     @staticmethod
-    def check_values_matches(question,value_one, value_two):
+    def check_values_matches(question, value_one, value_two):
         if value_one == value_two:
             return True
         else:
-            assert False, '"' + value_one + '" did not matched with "' + value_two + '" for question ' + question
+            assert False, '"' + value_one + '" did not match with "' + value_two + '" for question ' + question
 
     @staticmethod
-    def check_values_not_matches(value_one, value_two):
+    def check_values_not_matches(question, value_one, value_two):
         if value_one != value_two:
             return True
         else:
-            assert False, '"' + value_one + '" matched with "' + value_two + '"'
+            assert False, '"' + value_one + '" matches with "' + value_two + '" for question ' + question
