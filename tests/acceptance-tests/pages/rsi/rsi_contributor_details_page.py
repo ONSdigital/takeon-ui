69fb8ce6c5fb9bc717814b9d6cdd5ee197d2d5d1
Fayaz Mohammed <fayaz0211@gmail.com>
2020-06-19 15:14:40 +0100

    added the page object element, methods for value zero, blank conditions

--- tests/acceptance-tests/pages/rsi/rsi_contributor_details_page.py
+++ tests/acceptance-tests/pages/rsi/rsi_contributor_details_page.py
@@ -9,6 +9,7 @@ class RsiContributorDetailsPage(BasePage):
     QUESTION_ONE_ELEMENT = By.ID, '0020'
     QUESTION_TWO_ELEMENT = By.ID, '0021'
     QUESTION_NO_146 = By.ID, '0146'
+    QUESTION_TURNOVER_ELEMENT = By.ID, '0020'
     QUESTION_LABEL_PART_ONE = "//label[contains(text(),'"
     QUESTION_LABEL_PART_TWO = "')]"
 
@@ -27,12 +28,25 @@ class RsiContributorDetailsPage(BasePage):
         ContributorDetailsPage().save_the_application()
         SeleniumCore.close_the_current_window()
 
+    def submit_question_value(self, value_type, value, question):
+        if value_type == 'comment':
+            self.submit_comment_value(value, question)
+        elif value_type == 'total turnover':
+            self.submit_total_turnover_value(value, question)
+
     def submit_comment_value(self, comment, question):
         SeleniumCore.switch_window()
         if comment == 'empty':
             comment = ''
         if question.upper() == 'Q146':
             SeleniumCore.set_element_text(*RsiContributorDetailsPage.QUESTION_NO_146, comment)
+
+    def submit_total_turnover_value(self, value, question):
+        SeleniumCore.switch_window()
+        if value == 'blank':
+            value = ''
+        if question.upper() == 'Q20':
+            SeleniumCore.set_element_text(*RsiContributorDetailsPage.QUESTION_TURNOVER_ELEMENT, value)
 
     def validate_the_current_period_details(self, internet_sales):
         SeleniumCore.switch_window()
@@ -51,5 +65,8 @@ class RsiContributorDetailsPage(BasePage):
         else:
             return False
 
-    def check_comment_present_val_msg(self, exp_msg, is_val_exists):
-        ContributorDetailsPage().check_validation_message('Q146', exp_msg, is_val_exists)
+    # def check_comment_present_val_msg(self, exp_msg, is_val_exists):
+    #     ContributorDetailsPage().check_validation_message('Q146', exp_msg, is_val_exists)
+
+    # def check_validation_msg(self, question, exp_msg, is_val_exists):
+    #     ContributorDetailsPage().check_validation_message(question, exp_msg, is_val_exists)
