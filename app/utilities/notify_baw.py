6403da52937ecbae800d2a7738fe9dce5566d914
RyanAldy <ryanalderson91@gmail.com>
2020-07-31 11:44:09 +0100

    Amended syntax and added boto3 client

---
+++ app/utilities/notify_baw.py
@@ -0,0 +1,16 @@
+import json
+import os
+import boto3
+from flask import render_template, Blueprint
+from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
+from app.utilities.filter_validations import filter_validations
+from app.setup import log, api_caller
+from app.utilities.helpers import get_user
+
+def send_notification_to_queue(reference, period, survey):
+    save_notify_queue_url = 'https://sqs.eu-west-2.amazonaws.com/226575302242/dev-baw-save-notify'
+    notification_to_send = {"reference": reference, "period": period, "survey": survey}
+
+    sqs = boto3.client("sqs")
+    response = sqs.send_message(QueueUrl=save_notify_queue_url, MessageBody=json.dumps(notification_to_send))
+    return response
