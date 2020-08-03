525bb6096c781ff2d9f677a5e1df8d9b70f6c7a7
RyanAldy <ryanalderson91@gmail.com>
2020-08-03 15:57:12 +0100

    Amended boto3 client

--- app/utilities/notify_baw.py
+++ app/utilities/notify_baw.py
@@ -8,14 +8,14 @@ import boto3
 # from app.utilities.helpers import get_user
 
 def send_notification_to_queue(reference, period, survey):
-    session = boto3.Session(
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), 
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), 
-        region_name=os.getenv('AWS_DEFAULT_REGION'))
+    # session = boto3.Session(
+    #     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), 
+    #     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), 
+    #     region_name=os.getenv('AWS_DEFAULT_REGION'))
     save_notify_queue_url = 'https://sqs.eu-west-2.amazonaws.com/226575302242/dev-baw-save-notify'
     notification_to_send = {"reference": reference, "period": period, "survey": survey}
 
-    #sqs = boto3.client("sqs")
-    sqs = session.resource('sqs')
+    sqs = boto3.client("sqs", aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), region_name=os.getenv('AWS_DEFAULT_REGION'))
+    # sqs = session.resource('sqs')
     response = sqs.send_message(QueueUrl=save_notify_queue_url, MessageBody=json.dumps(notification_to_send))
     return response
