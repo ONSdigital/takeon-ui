88e494b07e5a88c3031ac72c4568ec0bb9d4bf64
RyanAldy <ryanalderson91@gmail.com>
2020-08-07 10:58:54 +0100

    Amended JSON Structure to send to BAW after discussions

--- app/utilities/notify_baw.py
+++ app/utilities/notify_baw.py
@@ -1,13 +1,19 @@
 import json
 import os
 from app.setup import log, api_caller
+from uuid import uuid1
 
 baw_notify_url = os.getenv('BAW_NOTIFY_URL')
 baw_notify_key = os.getenv('BAW_NOTIFY_KEY')
 
 def send_notification_to_queue(reference, period, survey):
 
-    notification_to_send = {"reference": reference, "period": period, "survey": survey}
+    notification_to_send = {
+        "response": str(uuid1()),
+        "reference": reference,
+        "period": period,
+        "survey": survey,
+        "duplicateFlag": False}
     header = {"x-api-key": baw_notify_key}
     response = api_caller.run_validation(
         baw_notify_url, json.dumps(notification_to_send), header)
