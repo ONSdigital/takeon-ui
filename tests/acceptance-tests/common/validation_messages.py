17c169814f5a2b59c37601af221a852edd88fbd4
Fayaz Mohammed <fayaz0211@gmail.com>
2020-08-17 12:13:38 +0100

    removed the extra lines

--- tests/acceptance-tests/common/validation_messages.py
+++ tests/acceptance-tests/common/validation_messages.py
@@ -6,8 +6,7 @@ class ValidationMessages:
         'popqvq validation': 'This has changed since last submission',
         'popzc validation': 'This is different to the previous submission. If this is 0 or blank, the previous was greater. If this has a value, the previous was 0 or blank',
         'qvq validation': 'This total is not equal to the derived total',
-        'qvv validation': 'There is a comment from this contributor',
-
+        'qvv validation': 'There is a comment from this contributor'
     }
 
     RSI_MESSAGES = {
@@ -22,7 +21,6 @@ class ValidationMessages:
         'vz validation': 'Total turnover(Q20) is zero',
         'vb and vz validation': 'Total turnover(Q20) is zero,Total turnover(Q20) is blank',
         'vpsic validation': 'SIC is petrol and internet sales(Q21) value is greater than zero'
-
     }
 
     TEST_SURVEY_MESSAGES = {
