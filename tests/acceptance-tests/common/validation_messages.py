4e2004f34ba95b03eacb4e87b0c32ff348d0e261
mattprice-28 <mattprice_28@hotmail.co.uk>
2020-09-02 09:51:56 +0100

    Added warning prefix to messages

--- tests/acceptance-tests/common/validation_messages.py
+++ tests/acceptance-tests/common/validation_messages.py
@@ -1,12 +1,12 @@
 class ValidationMessages:
     BMI_MESSAGES = {
-        'fv validation': 'Value set to default, please check',
-        'iv validation': 'This is not a valid brick type. It should be 2 (clay), 3 (concrete) or 4 (sandlime)',
-        'popm validation': 'This has changed significantly since the last submission',
-        'popqvq validation': 'This has changed since last submission',
-        'popzc validation': 'This is different to the previous submission. If this is 0 or blank, the previous was greater. If this has a value, the previous was 0 or blank',
-        'qvq validation': 'This total is not equal to the derived total',
-        'qvv validation': 'There is a comment from this contributor'
+        'fv validation': 'Warning - Value set to default, please check',
+        'iv validation': 'Warning - This is not a valid brick type. It should be 2 (clay), 3 (concrete) or 4 (sandlime)',
+        'popm validation': 'Warning - This has changed significantly since the last submission',
+        'popqvq validation': 'Warning - This has changed since last submission',
+        'popzc validation': 'Warning - This is different to the previous submission. If this is 0 or blank, the previous was greater. If this has a value, the previous was 0 or blank',
+        'qvq validation': 'Warning - This total is not equal to the derived total',
+        'qvv validation': 'Warning - There is a comment from this contributor'
     }
 
     RSI_MESSAGES = {
