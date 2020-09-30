c5a43bb9ca10aeb1e82d3945b69b4b20e98e7eff
Fayaz Mohammed <fayaz0211@gmail.com>
2020-09-30 13:10:55 +0100

    created new feature file for fixed value validation and added scenarios

--- tests/acceptance-tests/common/validation_messages.py
+++ tests/acceptance-tests/common/validation_messages.py
@@ -24,7 +24,9 @@ class ValidationMessages:
     }
 
     TEST_SURVEY_MESSAGES = {
-        'qvv validation': 'There is a comment from this contributor'
+        'qvv validation': 'There is a comment from this contributor',
+        'fv validation': 'Warning - Value set to default, please check',
+        'popmrz validation': ' Error - Internet value(Q21) = 0 this period but > 0 last period and ratio of Internet(Q21) to total turnover(Q20) was > 10% last period'
     }
 
     @staticmethod
