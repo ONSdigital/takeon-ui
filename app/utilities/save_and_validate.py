607b269019402bae59e9ac8c93505bf9769ef637
RyanAldy <ryanalderson91@gmail.com>
2020-10-07 16:39:53 +0100

    Removed redirect call in save and validate function

--- app/utilities/save_and_validate.py
+++ app/utilities/save_and_validate.py
@@ -93,6 +93,8 @@ def validate(inqcode, period, ruref):
     #     historic_data=historic_data_json,
     #     grouped_historic_data=grouped_historic_data)
     #return redirect(url_for('view_form.view_form'))
-    time.sleep(1)
+    # time.sleep(1)
     log.info("Got here after sleep")
-    return redirect(url_for("view_form", inqcode=inqcode, period=period, ruref=ruref))
+    # Doesn't need to do this
+    # return redirect(url_for("view_form", inqcode=inqcode, period=period, ruref=ruref))
+    return status_message
