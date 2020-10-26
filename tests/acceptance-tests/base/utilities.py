c9d0777780aad0692130e7563fe24e6d9252721d
Fayaz Mohammed <fayaz0211@gmail.com>
2020-10-26 15:46:56 +0000

    added screenshot utility methods whenever a scenario fails

--- tests/acceptance-tests/base/utilities.py
+++ tests/acceptance-tests/base/utilities.py
@@ -1,3 +1,9 @@
+import os
+import time
+
+from base.driver_context import DriverContext
+
+
 class Utilities:
 
     @staticmethod
@@ -23,3 +29,40 @@ class Utilities:
     @staticmethod
     def remove_multiple_spaces_in_a_string(value):
         return " ".join(value.split())
+
+    @staticmethod
+    def take_screen_shot(*scenario):
+        scenario_error_dir = Utilities.create_screen_shots_folder()
+        if scenario[0].status.name == 'failed':
+            # the element with longest height on page
+            scenario_file_path = os.path.join(scenario_error_dir,
+                                              scenario[0].feature.scenarios[0].name
+                                              + '_' + time.strftime("%H%M%S_%d_%m_%Y")
+                                              + '.png')
+            el = DriverContext.driver.find_element_by_tag_name('body')
+            el.screenshot(scenario_file_path)
+
+    @staticmethod
+    def create_screen_shots_folder():
+        scenario_error_dir = os.path.expanduser("~") + '/takeon-ui/tests/acceptance-tests/screen-shots'
+        if not os.path.exists(scenario_error_dir):
+            os.makedirs(scenario_error_dir)
+        else:
+            Utilities.delete_all_the_previous_screenshots(scenario_error_dir)
+        return scenario_error_dir
+
+    @staticmethod
+    def delete_all_the_previous_screenshots(folder_path):
+        for file in os.scandir(folder_path):
+            if file.name.endswith(".png"):
+                os.unlink(file.path)
+
+    @staticmethod
+    def get_current_page_title():
+        return DriverContext.driver.find_element_by_xpath("//div[contains(@class,'header__title')]").text
+
+    @staticmethod
+    def check_page_title(page_title):
+        title = Utilities.get_current_page_title()
+        if title.lower() != page_title.lower():
+            assert False, 'Expected page header title to be "' + page_title + '" But the current page header title is "' + element.text + '"'
