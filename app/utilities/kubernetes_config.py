1f53409f5152e6b6e476d199c0bccf18400d88d2
ranjit-das <Ranjit.Das@ons.gov.uk>
2021-01-22 15:59:14 +0000

    spp-887: used log.info in kubernetes_config.py

--- app/utilities/kubernetes_config.py
+++ app/utilities/kubernetes_config.py
@@ -9,7 +9,7 @@ class KubernetesConfig:
         config.load_incluster_config()  # This throws an exception on failure?
         self.client = client.CoreV1Api()
         self.namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
-        log.error('Namespace: {}'.format(self.namespace))
+        log.info('Namespace: {}'.format(self.namespace))
 
     def get_ip(self):
         service = self.client.read_namespaced_service(namespace=self.namespace, name=self.service_name)
