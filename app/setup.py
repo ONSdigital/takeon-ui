809838080dabfb7913729efe5c44a6291bec4338
ranjit-das <Ranjit.Das@ons.gov.uk>
2021-01-22 15:57:41 +0000

    spp-887: changes to add print to kubernetes_config.py

--- app/setup.py
+++ app/setup.py
@@ -45,9 +45,7 @@ def set_logger():
 
 log = set_logger()
 
-api_caller = ApiRequest(service="business-layer", mocking=settings.MOCKING)
-api_caller_pl = ApiRequest(service="persistence-layer", mocking=settings.MOCKING)
-
+api_caller = ApiRequest(log, service="business-layer", mocking=settings.MOCKING)
 
 def create_app(setting_overrides=None):
     # Define the WSGI application object
