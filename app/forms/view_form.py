b7a318cfb8c420b61acca1f20b03ad55af0167f6
RyanAldy <ryanalderson91@gmail.com>
2021-02-11 14:27:42 +0000

    Updated code for form submit

--- app/forms/view_form.py
+++ app/forms/view_form.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 from flask import render_template, Blueprint, request, redirect, url_for, current_app, abort
 from app.utilities.helpers import build_uri, get_user, question_order
 from app.utilities.filter_validations import filter_validations
@@ -79,6 +80,7 @@ def view_form(inqcode, period, ruref):
     if request.form and request.form['action'] == 'save-and-validate':
         save_form(parameters, request.form, inqcode, period, ruref)
         validate(inqcode, period, ruref)
+        time.sleep(5)
         return redirect(url_for('view_form.view_form', inqcode=inqcode, period=period, ruref=ruref))
 
     return render_template(
@@ -128,3 +130,4 @@ def override_all_button(data):
         if validation_triggered_counter == 2:
             return True
     return False
+
